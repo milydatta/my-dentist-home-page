29c165e9feb849b6b492dd9319ed2879ee1426ba
mily datta <dmily270@gmail.com>
2021-06-06 10:36:02 +0600

    some update

--- src/components/Login/Login/firebase.config.js
+++ src/components/Login/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAT2RFctO6XuXdhhYZWLeCJVYuVAzSXL1g",
-    authDomain: "burj-al-arab-16d68.firebaseapp.com",
-    projectId: "burj-al-arab-16d68",
-    storageBucket: "burj-al-arab-16d68.appspot.com",
-    messagingSenderId: "49945813727",
-    appId: "1:49945813727:web:2f6c253200686ebe17ff6a"
+  apiKey: "AIzaSyA2s2dCbZHuQX8YHUmrKvRcQahpPHbscLM",
+  authDomain: "my-dentist-71297.firebaseapp.com",
+  projectId: "my-dentist-71297",
+  storageBucket: "my-dentist-71297.appspot.com",
+  messagingSenderId: "104143849743",
+  appId: "1:104143849743:web:bb8cf26bb72ba4af48167a"
   };
 export default firebaseConfig;
