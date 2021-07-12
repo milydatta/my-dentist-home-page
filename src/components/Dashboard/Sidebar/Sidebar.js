fec9e7a4b35c2eb00a26782565793d0e05b2f27f
mily datta <dmily270@gmail.com>
2021-07-12 21:37:41 +0600

    some update

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -10,44 +10,45 @@ const Sidebar = () => {
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{height:"100vh"}}>
             <ul className="list-unstyled">
                 <li>
-                    <Link to="/dashboard" className="text-white">
+                    <Link to="/dashboard" className="text-white"style={{textDecoration:'none'}}>
                         <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span> 
                     </Link>
                 </li>
                 <li>
-                    <Link to="/" className="text-white">
+                    <Link to="/" className="text-white" style={{textDecoration:'none'}}>
                         <FontAwesomeIcon icon={faHome} /> <span>Home</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/appointment" className="text-white">
+                    <Link to="/appointment" className="text-white" style={{textDecoration:'none'}}>
                         <FontAwesomeIcon icon={faCalendar} /> <span>Appointment</span> 
                     </Link>
                 </li>
                 <li>
-                    <Link to="/allPatients" className="text-white">
+                    <Link to="/allPatients" className="text-white" style={{textDecoration:'none'}}>
                         <FontAwesomeIcon icon={faUsers} /> <span>AllPatients</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/prescriptions" className="text-white">
+                    <Link to="/prescriptions" className="text-white" style={{textDecoration:'none'}}>
                         <FontAwesomeIcon icon={faFileAlt} /> <span>Prescriptions</span>
                     </Link>
                 </li>
                 <li>
-                        <Link to="/addDoctor" className="text-white" >
+                        <Link to="/addDoctor" className="text-white" style={{textDecoration:'none'}}>
                             <FontAwesomeIcon icon={faUserPlus} /> <span>Add Doctor</span>
                         </Link>
                     </li>
    
                 <li>
-                    <Link to="/setting" className="text-white" >
+                    <Link to="/setting" className="text-white" style={{textDecoration:'none'}}>
                       <FontAwesomeIcon icon={faCog} /> <span>Setting</span>
                     </Link>
                 </li>
             </ul>
             <div>
-                <Link to="/" className="text-white"><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
+                <Link to="/" className="text-white" style={{textDecoration:'none'}}>
+                    <FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
             </div>
         </div>
     );
